91d0ae02d5dbc608d8c6784c5e124980ca1e2f72
HammyPig <jamesbwalden@protonmail.com>
2022-08-28 09:06:19 +1000

    feat: navigation

--- clubhub/src/App.js
+++ clubhub/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import ClubListPage from './ClubListPage.js'
 import EventListPage from './EventListPage.js'
+import ClubProfile from "./ClubProfile.js"
 import EventEditor from "./EventEditor.js"
 import { events } from "./events.js"
 import Homescreen from './Homescreen';
@@ -10,9 +12,18 @@ import Header from './Header';
 
 function App() {
   return (
-    <>
-    <Homescreen />
-    </>
+    <Router>
+      header
+      <Routes>
+        <Route path="/" element={<Homescreen />} />
+        <Route path="/clubs" element={<ClubListPage />} />
+        <Route path="/clubs/:id/events" element={<EventListPage />} />
+        <Route path="/clubs/:id/events/edit" element={<EventEditor />} />
+        <Route path="/clubs/:id" element={<ClubProfile />} />
+        {/* <Route path="*" element={<Error />}/> */}
+      </Routes>
+      footer
+    </Router>
   );
 }
 
--- clubhub/src/EventCard.js
+++ clubhub/src/EventCard.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
-class EventCard extends React.Component {
-    render() {
-        const {name, description, startDate, startTime, endDate, endTime} = this.props.club;
+function EventCard(props) {
+    const {clubId, eventId, name, description, startDate, startTime, endDate, endTime} = props.club;
+    let navigate = useNavigate();
 
-        return (
-            <>
-            <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <h2>{name}</h2>
-                <h3>{`${startDate} at ${startTime} - ${endDate} at ${endTime}`.toUpperCase()}</h3>
-                <p>{description}</p>
-            </div>
-            </>
-        );
-    }
+    return (
+        <>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5" onClick={() => {navigate(`/clubs/${clubId}/events/edit`); }}>
+            <h2>{name}</h2>
+            <h3>{`${startDate} at ${startTime} - ${endDate} at ${endTime}`.toUpperCase()}</h3>
+            <p>{description}</p>
+        </div>
+        </>
+    );
 }
 
 export default EventCard;
