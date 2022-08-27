b7a18405b709b4f1a93e051dd5fbf65c65b1002f
Anna <annardwn@gmail.com>
2022-08-27 23:10:00 +1000

    feat: homescreen

---
+++ clubhub/src/Homescreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import App from './App';
+import logo from './clubhub-logo.png';
+import './Homescreen.css'
+
+class Homescreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: 'Homescreen'
+    };
+  };
+
+  getClub = () => {
+    this.setState({route: 'ClubList'});
+    // console.log(this.state)
+    console.log("click")
+  };
+
+  render() {
+    const {route} = this.state;
+      return (
+      <>
+        { route === 'Homescreen'
+        ? <>
+          <div className='Header'>
+            <img src={logo} alt="clubhub-logo" />
+          </div>
+
+          <div className='Container'>
+            <p className='Sub'>Joining a society should be more than scrolling through a list of names.</p>
+            <button className='Primary-Button' onClick={this.getClub}>Find your club</button>
+          </div>
+
+        </>
+        : <App />
+        }
+
+      </>
+    );
+  }
+}
+
+export default Homescreen;
--- clubhub/src/index.js
+++ clubhub/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Homescreen from './Homescreen';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Homescreen />
   </React.StrictMode>
 );
 
